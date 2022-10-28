c6974508310b9ecd8be23b964e5bbf7e94a8014b
Malvina Hasa <hasamalvina24@gmail.com>
2022-10-28 18:36:32 -0400

    Added HTML routes for notes.html and index.html.

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('public'));
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 
 
