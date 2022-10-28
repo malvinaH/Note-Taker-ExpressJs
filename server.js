88a71736eb439e0070b336b2c683ab8fa489e8d3
Malvina Hasa <hasamalvina24@gmail.com>
2022-10-28 17:58:18 -0400

    Added server.js file with necessary packages for this project

---
+++ server.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
