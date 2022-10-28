b3340154d6474f15992ad292588824d3108d4e88
Malvina Hasa <hasamalvina24@gmail.com>
2022-10-28 18:48:10 -0400

    Added API route for GET /api/notes.

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
 app.use(express.static('public'));
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get('/api/notes', (req, res) => {
+    const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
+    const parseNotes = JSON.parse(dataNotes);
+    res.json(parseNotes);
 });
 
 app.get("*", (req, res) => {
