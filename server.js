e4679a9c8ebbcdc2ee746ba72019d4f1b2d9c3d9
Malvina Hasa <hasamalvina24@gmail.com>
2022-11-05 12:16:34 -0400

    rearranges routes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
-const dbJson = require('./db/db.json')
-var uuidv1 = require('uuidv1')
+const dbJson = require("./db/db.json")
+var uuidv1 = require("uuidv1")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,29 +10,32 @@ const PORT = process.env.PORT || 3001;
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // routes
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get('/api/notes', (req, res) => {
-    const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
+app.get("/api/notes", (req, res) => {
+    const dataNotes = fs.readFileSync(path.join(__dirname, "./db/db.json"), "utf-8");
     const parseNotes = JSON.parse(dataNotes);
     res.json(parseNotes);
 });
 
-app.post('/api/notes', (req, res) => {
-    const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
+app.post("/api/notes", (req, res) => {
+    const dataNotes = fs.readFileSync(path.join(__dirname, "./db/db.json"), "utf-8");
     const parseNotes = JSON.parse(dataNotes);
     req.body.id = uuidv1()
     parseNotes.push(req.body);
 
-    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(parseNotes), "utf-8");
+    fs.writeFileSync(path.join(__dirname, "./db/db.json"), JSON.stringify(parseNotes), "utf-8");
     res.json("You have successfully added a note!");
 });
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.delete("/api/notes/:id", function (req, res) {
     let deletedNote = parseInt(req.params.id);
@@ -49,10 +52,6 @@ app.delete("/api/notes/:id", function (req, res) {
             });
         }
     }
-});
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}!`));
