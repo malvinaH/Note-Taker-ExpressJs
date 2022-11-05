256d7d12f94fcfe093aaef704fe8f42acce1cb63
Malvina Hasa <hasamalvina24@gmail.com>
2022-11-05 11:39:03 -0400

    Added urlencoded to parse incoming string or array data

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
-
+const dbJson = require('./db/db.json')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
