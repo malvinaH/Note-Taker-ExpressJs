31347a71ac031a0bbc409661cad07a3c4223d0eb
Malvina Hasa <hasamalvina24@gmail.com>
2022-11-05 11:59:18 -0400

    Cleaned code for routes with one line

--- server.js
+++ server.js
@@ -2,18 +2,17 @@ const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const dbJson = require('./db/db.json')
+var uuidv1 = require('uuidv1')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
 
 app.get('/api/notes', (req, res) => {
     const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
@@ -31,10 +30,6 @@ app.post('/api/notes', (req, res) => {
     res.json("You have successfully added a note!");
 });
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-});
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}!`));
