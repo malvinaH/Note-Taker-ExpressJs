b0cd500d56b8ccb6040a3fd6f6c351aa619ed399
Malvina Hasa <hasamalvina24@gmail.com>
2022-11-05 12:02:08 -0400

    Testing some changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const path = require("path");
 const dbJson = require('./db/db.json')
-var uuidv1 = require('uuidv1')
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,7 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "./public/notes.html")));
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
 app.get('/api/notes', (req, res) => {
     const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
@@ -30,6 +32,8 @@ app.post('/api/notes', (req, res) => {
     res.json("You have successfully added a note!");
 });
 
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}!`));
